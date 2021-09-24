3903fcabda06e32b695328329b4055dcaa7ec9c1
Baptiste Coquelet <b.coquelet@eleve.leschartreux.net>
2021-09-24 15:54:06 +0200

    Initial commit

---
+++ app/models/User.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\models;
+
+class User{
+
+    private $nom;
+    private $prenom;
+    public function __construct($nom,$prenom="")
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+
+
+    }
+}
+?>
