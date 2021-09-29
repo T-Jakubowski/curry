2665505abc180b81b3dee00585271f016a3fad73
Baptiste Coquelet <b.coquelet@eleve.leschartreux.net>
2021-09-29 16:10:32 +0200

    Mise A jour SingletonDB et config

--- app/utils/SingletonDBMaria.php
+++ app/utils/SingletonDBMaria.php
@@ -4,21 +4,21 @@ namespace app\utils;
 
 
 class SingletonDBMaria{
-
     public $cnx;
     /** @var SingletonDBMaria */
     private static $instance;
-
-    private $dsn;//Data Source Name
+    private $dsn;
     private $username;
     private $password;
-
     private function __construct()
     {
+        $this->dsn=SingletonConfigReader::getInstance()->getValue("dsn");
+        $this->username=SingletonConfigReader::getInstance()->getValue("username");
+        $this->password=SingletonConfigReader::getInstance()->getValue("password");
+
         $this->cnx= new \PDO($this->dsn,$this->username,$this->password);
         $this->cnx->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
     }
-
     public static function getInstance() : SingletonDBMaria {
         if(is_null(self::$instance)){
             self::$instance = new SingletonDBMaria();
@@ -29,11 +29,5 @@ class SingletonDBMaria{
     {
         return $this->cnx;
     }
-
-
-
 }
-
-
-
 ?>
--- app/utils/SingletonLogger.php
+++ app/utils/SingletonLogger.php
@@ -1,49 +1,58 @@
 <?php
+
 namespace app\utils;
 
+use app\utils\SingletonConfigReader;
+
 /**
- * SingletonLogger class
- * Singleton using lazy instantiation
+ * A basic implementation of a logger for PHP
+ * with Singleton pattern... not the best way probably
+ * Should rewrite with a static class ??
+ * Please see PSR-3 at https://www.php-fig.org/psr/psr-3/
+ * and take a look to https://www.phpfacile.com/apprendre_le_php/logs_ou_traces_avec_monolog
  */
 class SingletonLogger
 {
-    private static $instance = NULL;
-    private $logs;
-
-    private function __construct() {
-        $logs = array();
-    }
+    /** @var  SingletonLogger */
+    private static $instance;
+    private string $logFile;
+    private bool $logOn; //Correct naming ??
+    /** @var resource */
+    private $handle;
 
     /**
-     * Gets instance of the SingletonLogger
-     * @return SingletonLogger instance
-     * @access public
+     * @throws \Exception
      */
-    public function getInstance() {
-        if(self::$instance === NULL) {
+    private function __construct()
+    {
+        //FIXME : set default value if no value in config file. All log files must be stored in /storage
+        $logFileName = SingletonConfigReader::getInstance()->getValue("logfile");
+        if ($logFileName =="") {
+            $logFileName="logger.log" ;
+        }
+        //This works only in a server context... Should we use the PROJECT_ROOT constant defined in autoloader ?
+        $this->logFile = $_SERVER["DOCUMENT_ROOT"] . "/../storage/" . $logFileName;
+        //FIXME : set default value to false (logger will be Off)
+        $this->logOn = SingletonConfigReader::getInstance()->getValue("logger") ?? 1 ;
+        //TODO : what happens if opening fail ?
+        $this->handle = fopen($this->logFile, "a");
+    }
+
+    public static function getInstance(): SingletonLogger
+    {
+        if (is_null(self::$instance)) {
             self::$instance = new SingletonLogger();
         }
         return self::$instance;
     }
 
-    /**
-     * Adds a message to the log
-     * @param String $message Message to be logged
-     * @access public
-     */
-    public function log($message) {
-        $this->logs[] = $message;
-    }
-
-    /**
-     * Returns array of logs
-     * @return array Array of log messages
-     * @access public
-     */
-    public function get_logs() {
-        return $this->logs;
+    public function log(string $message): void
+    {
+        //let's log the message with timestamp...
+        if ($this->logOn == true) {
+            //FIXME : date returns value for summer time for GMT (local time - 2 h) !!
+            //echo "[" . date('d-m-y h:i:s') . "] :\t" . $message ;
+             fwrite($this->handle, "[" . date('d-m-y h:i:s') . "] :\t" . $message . "\n");
+        }
     }
 }
-//class unique qui envoie les message derreur dans un fichier de log
-
-?>
