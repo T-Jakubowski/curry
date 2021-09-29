12cdaae1cb1666f28807f00500d3002289d6a0ec
Baptiste Coquelet <b.coquelet@eleve.leschartreux.net>
2021-09-29 09:35:38 +0200

    Maj caserne

--- app/models/Caserne.php
+++ app/models/Caserne.php
@@ -8,12 +8,50 @@ class Caserne{
    private $Ville;
    private $CodeTypeC;
 
-   public function __construct($NumCaserne,$Adresse="")
+   public function __construct($NumCaserne,$Adresse="",$CP="",$Ville="",$CodeTypeC)
    {
        $this->NumCaserne = $NumCaserne;
        $this->Adresse = $Adresse;
-
+       $this->CP = $CP;
+       $this->Ville = $Ville;
+       $this->CodeTypeC = $CodeTypeC;
    }
+    public function getNumCaserne(){
+        return $this->NumCaserne;
+    }
+    public function getAdresse(){
+        return $this->Adresse;
+    }
+    public function getCP(){
+        return $this->CP;
+    }
+    public function getVille(){
+        return $this->Ville;
+    }
+    public function getCodeTypeC(){
+        return $this->CodeTypeC;
+    }
+
+    public function setNumCaserne($NumCaserne){
+        $this->NumCaserne = $NumCaserne;
+        return $this;
+    }
+    public function setAdresse($Adresse){
+        $this->Adresse = $Adresse;
+        return $this;
+    }
+    public function setCP($CP){
+        $this->CP = $CP;
+        return $this;
+    }
+    public function setVille($Ville){
+        $this->Ville = $Ville;
+        return $this;
+    }
+    public function setNumCodeTypeC($CodeTypeC){
+        $this->CodeTypeC = $CodeTypeC;
+        return $this;
+    }
 }
 
 ?>
--- app/models/DAOCaserne.php
+++ app/models/DAOCaserne.php
@@ -1,36 +1,139 @@
 <?php
-
 namespace app\models;
+use PDO;
+use FFI\Exception;
+require_once "Caserne.php";
+use app\models\Caserne;
 
+$dbhost="127.0.0.1";
+$dbname="Pompier";
+$dbuser="Pompier_dbuser";
+$dbpassword="123123";
+//SOLID ET GRASP A cherché
+// loger et object de connexion unique
+
+try{
+    $cnx = new PDO("mysql:host=". $dbhost.";dbname=".$dbname, $dbuser, $dbpassword);
+}catch(Exception $e){
+    echo "<pre>" . print_r($e, true) ."</pre>";
+    } 
+
+
+
+/*$daop = new \app\models\DAOPompier($cnx);
+$nb = $daop->count();
+echo "nb pompier : ".$count;*/
 class DAOCaserne{
-
-    private $cnx;
-   public function __construct($connect)
+    private PDO $cnx;
+    
+   public function __construct()
    {
-       
+    
    }
 
+    /*
+        Renvoie une caserne par rapport a son id
+        @param int $id
+        @return caserne $data
+    */
     public function find($id) : Caserne {
+        try{
+            
+            $SQL = 'SELECT NumCaserne ,Adresse ,CP ,Ville ,CodeTypeC FROM casernes c 
+            WHERE NumCaserne=1;';
+            //$SQL='SELECT * FROM casernes WHERE Matricule=? ';
+            //$preparedStatement=$connect->query($SQL);
+            
+            $dbhost="127.0.0.1";
+            $dbname="Pompier";
+            $dbuser="Pompier_dbuser";
+            $dbpassword="123123";
+
+
+            try{
+                $cnx = new PDO("mysql:host=". $dbhost.";dbname=".$dbname, $dbuser, $dbpassword);
+            }catch(Exception $e){
+                echo "<pre>" . print_r($e, true) ."</pre>";
+                }
+
+            
+            $preparedStatement=$cnx->prepare($SQL);
+            //$preparedStatement->bindParam("?",$id);
+            $preparedStatement->execute();
+            var_dump($preparedStatement);
+            $data=$preparedStatement->fetchObject("Caserne");
+            var_dump($data);
+            return $data;
+            //$preparedStatement->bindValue(1,$mat); comme bindparam mais presque
+        }
+        catch(Exception $e)
+        {
+            echo "<pre>" . print_r($e, true) ."</pre>";
+        }
 
     }
-
-    public function save(Caserne $pompier) : void{
-
+    
+    
+    /*
+        Enregistre une caserne dans la bdd Toute les caserne
+        @param Caserne $Caserne
+        @return void
+    */
+    public function save(Caserne $Caserne) : void{
+        try{
+            $sql = 'SELECT COUNT(*) as nbPompiers from pompiers p ;';
+            $SQL='SELECT * FROM pompiers WHERE Matricule=? ';
+            //$statement=$cnx->query($SQL);
+            
+            //$preparedStatement=$cnx->prepare($SQL);
+            //$preparedStatement->bindParam(1,$mat);
+            //$preparedStatement->bindValue(1,$mat); comme bindparam mais presque
+        }catch(Exception $e){
+            echo "<pre>" . print_r($e, true) ."</pre>";
+            } 
     }
-
-    public function remove(Caserne $pompier) : void{ 
-        // supprimer object pompier plus dans la bdd
+    /*
+        Supprime une caserne
+        @param Caserne $Caserne
+        @return void
+    */
+    public function remove(Caserne $Caserne) : void{ 
+        // supprimer object caserne plus dans la bdd
         // 
     }
+    /*
+        Renvoie Toute les caserne
+        @param int $offset
+        @param int $limit
+        @return array<Caserne>
+    */
     public function findAll($offset=0,$limit=10) : Array{
-
+        $x=[];
+        return $x;
     }
+    /*
+        Renvoie le nombre de caserne
+        @return int
+    */
     public function count() : int {
+        $x=1;
+        return $x;
+
+
 
     }
-    public function findFireHouseFromFireman(Pompier $pompier) : Pompier {
+/*
+$pompiers = getCountPompiers();
+echo $pompiers['nbPompiers'] .'<br>';
+echo "<br>Done !";
+
+*/
 
     }
+    $c=new DAOCaserne($cnx);
+    $c->find(5);
+    
 
-}
+
+
 ?>
