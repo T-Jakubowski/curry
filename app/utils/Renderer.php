0c86a9281618ff6aad6c0d4248fd92465b15e05b
Baptiste Coquelet <b.coquelet@eleve.leschartreux.net>
2021-10-03 17:57:09 +0200

    Ajout Vue Caserne

---
+++ app/utils/Renderer.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\utils;
+
+class Renderer{
+    public static function render($file,array $data=null) : string {
+        $path='../app/view/demo.php';//chemin vers le fichier
+        ob_start();
+        if($data != null){
+            extract($data);
+        }
+        include $path;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+}
+
+?>
---
+++ app/view/home.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\views;
+//FAIRE PAGE AVEC BARRE EN HAUT
+//bouton caserne et pompier
+
+
+//LISTE CASERNES
+
+
+
+
+//LISTE POMPIERS
+//en + recherche par nom
+//Systeme de pagination (en bas)
+//bouton en bout de ligne pour supprimer pompier + confirmation
+//Bouton pour éditer
+
+?>
