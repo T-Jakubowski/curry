116e8ef0914426aa4ce8743691287500ea96c305
T.Jakubowski <t.jakubowski@eleve.leschartreux.net>
2021-11-08 15:41:35 +0100

    Modification User dump

--- sql/dump_pompierDataBase_Init.sql
+++ sql/dump_pompierDataBase_Init.sql
@@ -3954,12 +3954,11 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 Create table user(
-    Id INT NOT NULL AUTO_INCREMENT,
-    Identifiant varchar(20),
-    Password varchar(20),
-    IdRole int,
-    primary key (Id),
-    foreign key (IdRole) references role(Id)
+	Identifiant varchar(20) PRIMARY KEY NOT NULL,
+	Prenom varchar(20),
+	Nom varchar(20),
+	IdRole int,
+	foreign key (IdRole) references role(Id)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
