734d7c83184de144e3545f708b0795f95811c50b
T.Jakubowski <t.jakubowski@eleve.leschartreux.net>
2021-11-19 11:55:02 +0100

    Un utilisateur ne peut plus se supprimer lui même

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -19,35 +19,34 @@ class UserController extends BaseController {
         $this->DAOUser = $DAOUser;
     }
 
-
     //renvoie la page avec la liste de tout les pompier
     public function show() {
         $auth = new Auth();
-        $isactive = $auth->is_session_active();
-        if ($isactive == true) {
-            $managePerm = $auth->can('manage');
-            if ($managePerm == true) {
-                if (isset($_GET["page"])) {
-                    $Offset = ($_GET["page"] * 10) - 10;
-                } else {
-                    $Offset = 0;
-                }
-                if (isset($_GET["search"])) {
-                    $NumU = ($_GET["search"]);
-                    $LstUser = $this->DAOUser->findAllWhereIdentifiant($NumU, 0, 10);
-                    $CountUser = $this->DAOUser->countWhere($NumU);
-                } else {
-                    $LstUser = $this->DAOUser->findAll($Offset, 10);
-                    $CountUser = $this->DAOUser->count();
-                }
-                $readPerm = $auth->can('manage');
-                $page = Renderer::render("view_user.php", compact("LstUser", "CountUser"));
-            }else{
-                $page = Renderer::render("view_denyAccess.php");
-            }
+          $isactive = $auth->is_session_active();
+          if ($isactive == true) {
+          $managePerm = $auth->can('manage');
+          if ($managePerm == true) {
+        if (isset($_GET["page"])) {
+            $Offset = ($_GET["page"] * 10) - 10;
         } else {
-            $page = Renderer::render("view_login.php");
+            $Offset = 0;
         }
+        if (isset($_GET["search"])) {
+            $NumU = ($_GET["search"]);
+            $LstUser = $this->DAOUser->findAllWhereIdentifiant($NumU, 0, 10);
+            $CountUser = $this->DAOUser->countWhere($NumU);
+        } else {
+            $LstUser = $this->DAOUser->findAll($Offset, 10);
+            $CountUser = $this->DAOUser->count();
+        }
+        $readPerm = $auth->can('manage');
+        $page = Renderer::render("view_user.php", compact("LstUser", "CountUser"));
+        } else {
+          $page = Renderer::render("view_denyAccess.php");
+          }
+          } else {
+          $page = Renderer::render("view_login.php");
+          }
         echo $page;
     }
 
@@ -57,12 +56,65 @@ class UserController extends BaseController {
         $isactive = $auth->is_session_active();
         if ($isactive == true) {
             $managePerm = $auth->can('manage');
-                if ($managePerm == true) {
-                    $identifiant = htmlspecialchars($_POST['addidentifiant']);
-                    $nom = htmlspecialchars($_POST['addnom']);
-                    $prenom = htmlspecialchars($_POST['addprenom']);
-                    $password = htmlspecialchars($_POST['addpassword']);
-                    $role = htmlspecialchars($_POST['addidrole']);
+            if ($managePerm == true) {
+                $identifiant = htmlspecialchars($_POST['addidentifiant']);
+                $nom = htmlspecialchars($_POST['addnom']);
+                $prenom = htmlspecialchars($_POST['addprenom']);
+                $password = htmlspecialchars($_POST['addpassword']);
+                $role = htmlspecialchars($_POST['addidrole']);
+                $data = array(
+                    "identifiant" => $identifiant,
+                    "nom" => $nom,
+                    "prenom" => $prenom,
+                    "password" => $password,
+                    "idRole" => $role,
+                );
+                $f = new FiltreUser($data);
+                $data = $f->use();
+                $isSuccess = true;
+                foreach ($data as $key => $value) {
+                    if ($value == false) {
+                        $isSuccess = false;
+                        $valueError[] = $key;
+                    }
+                }
+                if ($isSuccess == true) {
+                    $password = hash('sha256', $password);
+                    $userToAdd = new User($identifiant, $nom, $prenom, $password, $role);
+                    $this->DAOUser->save($userToAdd);
+                    $resultMessage = "le user numéro a bien été ajouter";
+                    $page = Renderer::render("view_user_add.php", compact("resultMessage"));
+                } else {
+                    $page = Renderer::render("view_user_add.php", compact("valueError"));
+                }
+            } else {
+                $page = Renderer::render("view_denyAccess.php");
+            }
+        } else {
+            $page = Renderer::render("view_login.php");
+        }
+        echo $page;
+    }
+
+    /*
+      Modifie un user
+      @return void
+     */
+
+    public function update(): void {
+        $auth = new Auth();
+        $isactive = $auth->is_session_active();
+        if ($isactive == true) {
+            $managePerm = $auth->can('manage');
+            if ($managePerm == true) {
+                $identifiant = htmlspecialchars($_POST['editidentifiant']);
+                $isExist = $this->DAOUser->findIfUserIdentifiantExist($identifiant);
+                $isSuccess = false;
+                if ($isExist == true) {
+                    $nom = htmlspecialchars($_POST['editnom']);
+                    $prenom = htmlspecialchars($_POST['editprenom']);
+                    $password = htmlspecialchars($_POST['editpassword']);
+                    $role = htmlspecialchars($_POST['editidrole']);
                     $data = array(
                         "identifiant" => $identifiant,
                         "nom" => $nom,
@@ -75,95 +127,45 @@ class UserController extends BaseController {
                     $isSuccess = true;
                     foreach ($data as $key => $value) {
                         if ($value == false) {
-                            $isSuccess = false;
-                            $valueError[] = $key;
-                            
-                        }
-                    }
-                    if ($isSuccess == true) {
-                        $password=hash('sha256', $password);
-                        $userToAdd = new User($identifiant, $nom, $prenom, $password, $role);
-                        $this->DAOUser->save($userToAdd);
-                        $resultMessage = "le user numéro a bien été ajouter";
-                        $page = Renderer::render("view_user_add.php", compact("resultMessage"));
-                    } else {
-                        $page = Renderer::render("view_user_add.php", compact("valueError"));
-                    }            
-                }else{
-                    $page = Renderer::render("view_denyAccess.php");
-                }
-            } else {
-                $page = Renderer::render("view_login.php");
-            }
-        echo $page;
-    }
-
-    /*
-        Modifie un user
-        @return void
-    */
-    public function update(): void {
-        $auth = new Auth();
-        $isactive = $auth->is_session_active();
-        if ($isactive == true) {
-            $managePerm = $auth->can('manage');
-                if ($managePerm == true) {
-                    $identifiant = htmlspecialchars($_POST['editidentifiant']);
-                    $isExist = $this->DAOUser->findIfUserIdentifiantExist($identifiant);
-                    $isSuccess = false;
-                    if ($isExist == true) {
-                        $nom = htmlspecialchars($_POST['editnom']);
-                        $prenom = htmlspecialchars($_POST['editprenom']);
-                        $password = htmlspecialchars($_POST['editpassword']);
-                        $role = htmlspecialchars($_POST['editidrole']);
-                        $data = array(
-                            "identifiant" => $identifiant,
-                            "nom" => $nom,
-                            "prenom" => $prenom,
-                            "password" => $password,
-                            "idRole" => $role,
-                        );
-                        $f = new FiltreUser($data);
-                        $data = $f->use();
-                        $isSuccess = true;
-                        foreach ($data as $key => $value) {
-                            if ($value == false) {
-                                if ($key != "identifiant") {
-                                    $isSuccess = false;
-                                    $valueError[] = $key;
-                                }
+                            if ($key != "identifiant") {
+                                $isSuccess = false;
+                                $valueError[] = $key;
                             }
                         }
                     }
-                    if ($isSuccess == true) {
-                        $password=hash('sha256', $password);
-                        $userToUpdate = new User($identifiant, $nom, $prenom, $password, $role);
-                        $this->DAOUser->edit($userToUpdate);
-                        $resultMessage = "le user numéro '" . $identifiant . "' a bien été modifier";
-                        $page = Renderer::render("view_user_edit.php", compact("resultMessage"));
-                    } else {
-                        $page = Renderer::render("view_user_edit.php", compact("valueError"));
-                    }               
-                }else{
-                    $page = Renderer::render("view_denyAccess.php");
+                }
+                if ($isSuccess == true) {
+                    $password = hash('sha256', $password);
+                    $userToUpdate = new User($identifiant, $nom, $prenom, $password, $role);
+                    $this->DAOUser->edit($userToUpdate);
+                    $resultMessage = "le user numéro '" . $identifiant . "' a bien été modifier";
+                    $page = Renderer::render("view_user_edit.php", compact("resultMessage"));
+                } else {
+                    $page = Renderer::render("view_user_edit.php", compact("valueError"));
                 }
             } else {
-                $page = Renderer::render("view_login.php");
+                $page = Renderer::render("view_denyAccess.php");
             }
+        } else {
+            $page = Renderer::render("view_login.php");
+        }
         echo $page;
     }
 
     /*
-        Supprime un user
-        @return void
-    */
+      Supprime un user
+      @return void
+     */
+
     public function delete(): void {
         $auth = new Auth();
         $isactive = $auth->is_session_active();
         if ($isactive == true) {
             $mangagePerm = $auth->can('manage');
-                if ($mangagePerm == true) {
-                    $identifiant = htmlspecialchars($_POST['idUserToDelete']);
+            if ($mangagePerm == true) {
+                $identifiant = htmlspecialchars($_POST['idUserToDelete']);
+                $identifiant_session = $_SESSION['identifiant'];
+                if ($identifiant != $identifiant_session) {
                     $isExist = $this->DAOUser->findifUserIdentifiantExist($identifiant);
                     if ($isExist == true) {
                         $resultMessage = "le user numéro '" . $identifiant . "' a bien été Supprimer";
@@ -172,13 +174,17 @@ class UserController extends BaseController {
                     } else {
                         $valueError = "Vous ne pouvez pas supprimer le user " . $identifiant . " pour l'instant";
                         $page = Renderer::render("view_user_delete.php", compact("valueError"));
-                    }                
-                }else{
-                    $page = Renderer::render("view_denyAccess.php");
+                    }
+                } else {
+                    $valueError = "Vous ne pouvez pas vous supprimer vous même";
+                    $page = Renderer::render("view_user_delete.php", compact("valueError"));
                 }
             } else {
-                $page = Renderer::render("view_login.php");
+                $page = Renderer::render("view_denyAccess.php");
             }
+        } else {
+            $page = Renderer::render("view_login.php");
+        }
         echo $page;
     }
 
